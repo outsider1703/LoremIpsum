fea204828c083e674ab2b1e21984e7b3cfeaffbb
ingvar <simka1414@gmail.com>
2024-09-11 08:50:54 +0200

    Initial Commit

---
+++ LoremIpsum/LoremIpsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremIpsumApp.swift
+//  LoremIpsum
+//
+//  Created by ingvar on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoremIpsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
